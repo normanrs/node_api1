604241276cd0a65da7e66031750ad648c65a7378
Norman Schultz <normanrs@live.com>
2022-01-19 13:40:04 -0500

    Create a case-insensitive city search endpoint

--- test/integration/integrationRequests.test.js
+++ test/integration/integrationRequests.test.js
@@ -9,9 +9,23 @@ describe('Integration tests:', () => {
   });
 
   test('should return a healthcheck response', async () => {
-    const resp = await http.get('/healthcheck').set('Accept', 'application/json');
+    const resp = await http.get('/healthcheck');
     expect(resp.status).toEqual(200);
     expect(resp.body.healthy);
+  });
+
+  test('should return a city at random response', async () => {
+    const resp = await http.get('/randomCity');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toHaveProperty('name');
+  });
+
+  test('should return cities matching search', async () => {
+    const testQuery = {name: 'holt', limit: 20}
+    const resp = await http.get('/citySearch')
+        .query(testQuery);
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toBeInstanceOf(Array);
   });
 
 });
