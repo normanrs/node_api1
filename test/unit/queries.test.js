fb1429b4f46152b46bdf2cbb584d5e933d282c2c
Norman Schultz <normanrs@live.com>
2022-01-21 11:30:30 -0500

    Improve unit test coverage

--- test/unit/queries.test.js
+++ test/unit/queries.test.js
@@ -3,9 +3,25 @@ jest.mock('../../lib/db');
 
 describe('queries', () => {
 
-    it('should throw error on no db connection', async () => {
+    it('healthcheck should throw error on no db connection', async () => {
         try {
             await queries.healthcheck();
+          } catch (err) {
+            expect(err).toHaveProperty('stack');
+          }
+    });
+
+    it('randomCity should throw error on no db connection', async () => {
+        try {
+            await queries.randomCity();
+          } catch (err) {
+            expect(err).toHaveProperty('stack');
+          }
+    });
+
+    it('citySearch should throw error on no db connection', async () => {
+        try {
+            await queries.citySearch();
           } catch (err) {
             expect(err).toHaveProperty('stack');
           }
